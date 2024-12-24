767e07b061d3f593cfdee3763edea1d15bc4a9cd
sammcmurtrey <mcmurtreysamantha@gmail.com>
2024-12-23 21:22:37 -0600

    tset

---
+++ test.java
@@ -0,0 +1,8 @@
+import static java.lang.System.out;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("testing");
+    }
+}
